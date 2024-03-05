836e7c06efb621bc2a11bc3c4150c0061cb245ea
Samuel_Bruno <samuel@statustech.app>
2024-03-05 14:24:41 -0300

    Meta defs

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,6 +1,8 @@
 import { ReactComponent as GooglePlay } from "./files/google_play.svg"
 import { ReactComponent as MainLogo } from "./files/logo_full.svg"
 import { ReactComponent as LogoWhite } from "./files/logo_white.svg"
+import { ReactComponent as IconWhite } from "./files/icon_white.svg"
+import { ReactComponent as IconColorful } from "./files/icon_colorful.svg"
 import { ReactComponent as MinusIcon } from "./files/minus.svg"
 import { ReactComponent as AppStore } from "./files/app_store.svg"
 import { ReactComponent as PlusIcon } from "./files/plus.svg"
@@ -9,6 +11,8 @@ export {
   GooglePlay,
   MainLogo,
   LogoWhite,
+  IconWhite,  
+  IconColorful,
   MinusIcon,
   AppStore,
   PlusIcon,
